4e37d7f525eead4d07eeeb18856b1feb44bb17aa
Michael Nisi <michael.nisi@gmail.com>
2017-09-16 17:22:04 +0200

    Use https for dependency

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
   name: "Skull",
   dependencies: [
-    .Package(url: "git@github.com:michaelnisi/csqlite.git", majorVersion: 1)
+    .Package(url: "https://github.com/michaelnisi/csqlite.git", majorVersion: 1)
   ]
 )
