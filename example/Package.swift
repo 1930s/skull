43d46515f119fe8fcd968a7b77c0d62503b5dc42
Michael Nisi <michael.nisi@gmail.com>
2017-09-29 07:48:59 +0200

    Adjust example

--- example/Package.swift
+++ example/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
   name: "example",
   dependencies: [
-    .package(url: "../", from: "4.0.0")
+    .package(url: "https://github.com/michaelnisi/skull", from: "5.0.0")
   ],
   targets: [
     .target( name: "example", dependencies: ["Skull"])]
