4884881897cf5a857d5ffc004156074b4f097e21
Michael Nisi <michael.nisi@gmail.com>
2018-09-24 14:28:16 +0200

    Remove redundant directory

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     .library(name: "Skull", targets: ["Skull"])
   ],
   targets: [
-    .systemLibrary(name: "CSqlite3", path: "Libraries/csqlite"),
+    .systemLibrary(name: "CSqlite3", path: "module/macosx"),
     .target(name: "Skull", dependencies: ["CSqlite3"], path: "Sources"),
     .testTarget(name: "SkullTests", dependencies: ["Skull"])
   ],
